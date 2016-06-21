8c260f521cbed72382bace591951176aba53ee73
gary boland <garyboland@gmail.com>
2016-06-21 21:32:49 +0100

    HelloWait.java

---
+++ HelloWait.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] args) 
+        throws InterruptedException {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, Wait");
+        Thread.sleep(1000);
+        System.out.println("Goodbye!");
+    }
+
+}
+
