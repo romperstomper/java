94b84379270eb0165b1533f3bfb1c163afe9620e
gary boland <garyboland@gmail.com>
2016-06-21 22:05:28 +0100

    hellowait with arg parse

--- HelloWait.java
+++ HelloWait.java
@@ -2,9 +2,15 @@ public class HelloWait {
 
     public static void main(String[] args) 
         throws InterruptedException {
+      int time = 1000;
+      int num;
+      if (args.length > 0){
+        num = Integer.parseInt(args[0]);
+	time = 1000 * num;
+      }
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, Wait");
-        Thread.sleep(1000);
+        Thread.sleep(time);
         System.out.println("Goodbye!");
     }
 
