972739f12918469c4a28d031f3239b2fbf7ea114
gary boland <garyboland@gmail.com>
2016-06-25 12:34:15 +0100

    jolokia javaagent option

---
+++ wait.sh
@@ -0,0 +1,7 @@
+java\
+ -javaagent:jolokia-jvm-1.3.3-agent.jar\
+ -Dcom.sun.management.jmxremote.port=9010\
+ -Dcom.sun.management.jmxremote.authenticate=false\
+ -Dcom.sun.management.jmxremote -jar ~/jv/HelloWait.jar 2900 &
+
+
